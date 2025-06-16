015f21647e82ed913e0882fa39e6d4cea6891c6c
Andrew <andrew@defran.co>
2025-06-16 15:23:30 -0400

    Revert "merge"
    This reverts commit 170617addfac73f8495b73d340a07e7b9137fd19.

--- applier/lib/helpers/browser.ex
+++ applier/lib/helpers/browser.ex
@@ -4,64 +4,23 @@ defmodule Helpers.Browser do
   """
 
   def launch_and_navigate(url) do
-    case launch() do
-      {:ok, browser} ->
-        case create_page(browser) do
-          {:ok, page} ->
-            case navigate(page, url) do
-              :ok -> {:ok, browser, page}
-              error -> 
-                close_page(page)
-                close_browser(browser)
-                error
-            end
-          error ->
-            close_browser(browser)
-            error
-        end
-      error -> error
-    end
-  end
-
-  def launch do
     case Playwright.launch(:chromium, %{headless: false}) do
-      {:ok, browser} -> {:ok, browser}
-      {:error, reason} -> {:error, "Failed to start Playwright: #{inspect(reason)}"}
-    end
-  end
+      {:ok, browser} ->
+        page = Playwright.Browser.new_page(browser)
+        Playwright.Page.goto(page, url)
+        Process.sleep(2000)
+        {:ok, browser, page}
 
-  def create_page(browser) do
-    try do
-      page = Playwright.Browser.new_page(browser)
-      {:ok, page}
-    rescue
-      error -> {:error, "Failed to create page: #{inspect(error)}"}
-    end
-  end
-
-  def navigate(page, url) do
-    try do
-      Playwright.Page.goto(page, url)
-      Process.sleep(2000)
-      :ok
-    rescue
-      error -> {:error, "Failed to navigate to #{url}: #{inspect(error)}"}
+      {:error, reason} ->
+        {:error, "Failed to start Playwright: #{inspect(reason)}"}
     end
   end
 
   def close_browser(browser) do
-    try do
-      Playwright.Browser.close(browser)
-    rescue
-      _error -> :ok
-    end
+    Playwright.Browser.close(browser)
   end
 
   def close_page(page) do
-    try do
-      Playwright.Page.close(page)
-    rescue
-      _error -> :ok
-    end
+    Playwright.Page.close(page)
   end
 end
